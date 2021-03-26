60778ab4a79fefe86b9764a5dbde25b2a61b488f
joferava <jfelipe221095@gmail.com>
2021-03-26 13:16:16 -0600

    Initial Commit

---
+++ GeneracionToken/GeneracionTokenApp.swift
@@ -0,0 +1,17 @@
+//
+//  GeneracionTokenApp.swift
+//  GeneracionToken
+//
+//  Created by Felipe Ramirez Vargas on 26/3/21.
+//
+
+import SwiftUI
+
+@main
+struct GeneracionTokenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
